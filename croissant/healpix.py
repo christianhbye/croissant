525952903b8a089ed36edb775dac74c866234b75
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-10 16:57:34 -0700

    add alm class

--- croissant/healpix.py
+++ croissant/healpix.py
@@ -7,24 +7,23 @@ def nside2npix(nside):
     npix = 12 * nside**2
     return npix
 
-def check_shapes(npix, data, alm, frequencies):
-    data_check = True
-    alm_check = True
-    
+def check_shapes(npix, data, frequencies):
+    if data is None:
+        return
+
     if frequencies is None:
-        expected_data_shape = (npix,)
+        expected_shape = (npix,)
     else:
         nfreq = len(frequencies)
-        expected_data_shape = (nfreq, npix)
-        if alm is not None:
-            alm_check = np.shape(alm)[0] == nfreq
+        expected_shape = (nfreq, npix)
         
+    check = np.shape(data) == expected_shape
 
-    if data is not None:
-        data_check = np.shape(data) == expected_data_shape
-
-    if not (data_check and alm_check):
-        raise ValueError("Shape mismatch in npix, data, alm, frequencies.")
+    if not check:
+        raise ValueError(
+                f"Expected data shape is {expected_shape}, but data has"
+                f"shape {np.shape(data)}."
+            )
 
 # nside's for which pixel weights exist
 PIX_WEIGHTS_NSIDE = [32, 64, 128, 256, 512, 1024, 2048, 4096]
@@ -36,55 +35,87 @@ class HealpixBase:
             nside,
             data=None,
             nested_input=False,
-            alm=None,
             frequencies=None
         ):
         hp.check_nside(nside, nest=nested_input)
         self.nside = nside
-        self.npix = nside2npix(self.nside)
-        check_shapes(self.npix, data, alm, frequencies)
+        check_shapes(self.npix, data, frequencies)
 
-        if data is not None:
-            if nested_input:
-                ix = hp.nest2ring(self.nside, np.arange(self.npix))
-                if frequencies is None:
-                    data = data[ix]
-                else:
-                    data = data[:, ix]
-            if alm is None:
-                alm = self.hp2alm()
-            elif not np.allclose(alm, self.hp2alm()):
-                raise ValueError("Provided alm and map are inconsistent.")
-        else:
-            if alm is not None:
-                data = self.alm2hp()
+        if data is None:
+            nested_input = False
+
+        if nested_input:
+            ix = hp.nest2ring(self.nside, np.arange(self.npix))
+            if frequencies is None:
+                data = data[ix]
+            else:
+                data = data[:, ix]
 
         self.data = data
-        self.alm = alm
         self.frequencies = frequencies
 
-    def alm2hp(self, lmax=None):
-        if self.alm is None:
-            raise ValueError("alm is None, cannot convert to map.")
-        hp_map = hp.alm2map(self.alm, self.nside, lmax=lmax, mmax=lmax)
-        return hp_map
+    @property
+    def npix(self):
+        return nside2npix(self.nside)
 
-    def alm2hp(self, lmax=None):
-        if self.data is None:
+    @classmethod
+    def from_alm(cls, alm_obj, nside=None):
+        if nside is None:
+            nside = (lmax + 1) // 3
+        lmax = alm_obj.lmax
+        hp_map = hp.alm2map(alm_obj.alm, nside, lmax=lmax, mmax=lmax)
+        return cls(nside, data=hp_map, frequencies=alm_obj.frequencies)
+
+
+    def ud_grade(self, nside_out, **kwargs):
+        new_map = hp.ud_grade(self.data, nside_out, **kwargs)
+        self.data = new_map
+        self.nside = nside_out
+
+    def plot(self, **kwargs):
+        m = kwargs.pop("m", self.data)
+        _ = hp.projview(m=m, projection_type=projection)
+
+
+class Alm(hp.Alm):
+
+    def __init__(self, alm=None, lmax=None, frequencies=None):
+        if alm is not None and frequencies is not None:
+            if not np.shape(alm)[0] == len(frequencies)
+                raise ValueError("Shapes don't match: alms and frequencies.")
+
+        self.alm = alm
+        self.lmax = lmax
+        self.frequencies = frequencies
+
+    @classmethod
+    def from_healpix(cls, hp_obj, lmax=None): 
+        if hp_obj.data is None:
             raise ValueError("data is None, cannot compute alms.")
         if lmax is None:
-            lmax = 3 * self.nside - 1
-        use_pix_weights = self.nside in PIX_WEIGHTS_NSIDE
+            lmax = 3 * hp_obj.nside - 1
+        use_pix_weights = hp_obj.nside in PIX_WEIGHTS_NSIDE
         use_ring_weights = not use_pix_weights
         alm = hp.map2alm(
-            self.data,
+            hp_obj.data,
             lmax=lmax,
             mmax=lmax,
             use_ring_weights=use_ring_weights,
             use_pixel_weights=use_pix_weights,
         )
-        return alm
+        return cls(alm=alm, lmax=lmax, frequencies=hp_obj.frequencies)
 
-    def plot(self, **kwargs):
-        m = kwargs.pop("m", self.data)
-        _ = hp.projview(m=m, projection_type=projection)
+    def getlm(self, i=None):
+        return super().getlm(self.lmax, i=i)
+
+    def getidx(self, l, m):
+        return super().getidx(self.lmax, l, m)
+
+    @property
+    def size(self):
+        return super().getsize(self.lmax, mmax=self.lmax)
+
+    @property
+    def getlmax(self):
+        return self.lmax
+
