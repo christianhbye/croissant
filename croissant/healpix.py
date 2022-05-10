93d63cf096a955f970d124862153b2c13dd404bd
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-10 11:58:43 -0700

    clean up code, fix bugs

--- croissant/healpix.py
+++ croissant/healpix.py
@@ -1,11 +1,16 @@
 import healpy as hp
+import numpy as np
+import warnings
+
 
 def nside2npix(nside):
-    npix = 12 * nside **2
+    npix = 12 * nside**2
     return npix
+
 
 # nside's for which pixel weights exist
 PIX_WEIGHTS_NSIDE = [32, 64, 128, 256, 512, 1024, 2048, 4096]
+
 
 class HealpixBase:
     def __init__(self, nside, data=None, alm=None, nested_input=False):
@@ -13,17 +18,21 @@ class HealpixBase:
         self.nside = nside
         self.npix = nside2npix(self.nside)
         if nested_input and data is not None:
-            ix = hp.nest2ring(self.nside, np.arange(npix))
+            ix = hp.nest2ring(self.nside, np.arange(self.npix))
             data = data[ix]
         if data is None:
             if alm is not None:
-                data = alm2hp(alm)
+                data = self.alm2hp()
         else:
             if alm is None:
-                alm = hp2alm(data)
+                alm = self.hp2alm()
             else:
-                # consistency check
-                # np.allclose(alm, hp2alm(data))
+                if not np.allclose(alm, self.hp2alm()):
+                    warnings.warn(
+                        "Both a map and a list of alms are provided but"
+                        "they are not consistent.",
+                        UserWarning,
+                    )
 
         self.data = data
         self.alm = alm
@@ -40,29 +49,29 @@ class HealpixBase:
         if lmax is None:
             lmax = 3 * self.nside - 1
         use_pix_weights = self.nside in PIX_WEIGHTS_NSIDE
-        use_ring_weights = not use_pix_weigths
-        alm =  hp.map2alm(
-                self.data,
-                lmax=lmax,
-                mmax=lmax,
-                use_ring_weights=use_ring_weights,
-                use_pixel_weights=use_pix_weights,
-            )
+        use_ring_weights = not use_pix_weights
+        alm = hp.map2alm(
+            self.data,
+            lmax=lmax,
+            mmax=lmax,
+            use_ring_weights=use_ring_weights,
+            use_pixel_weights=use_pix_weights,
+        )
         return alm
 
     def plot(self, projection="mollweide", **kwargs):
         if self.data is None:
             raise ValueError("data is None, there is nothing to plot.")
         views = {
-                "mollweide": hp.mollview,
-                "gnomonic": hp.gnomview,
-                "cartesian": hp.cartview,
-                "ortographic": hp.orthview,
-                }
+            "mollweide": hp.mollview,
+            "gnomonic": hp.gnomview,
+            "cartesian": hp.cartview,
+            "ortographic": hp.orthview,
+        }
         if not projection in views:
             raise ValueError(
-                    f"Projection must be in {list[views.keys()]}, not"
-                    f"{projection}."
+                f"Projection must be in {list[views.keys()]}, not"
+                f"{projection}."
             )
         func = views[projection]
         func(self.data, **kwargs)
