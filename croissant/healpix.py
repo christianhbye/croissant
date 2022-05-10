8c271ee27b1d4f7d08bc4439129570a5b3d7a13f
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-10 15:31:48 -0700

    add freq dimension and consistency checks for shapes

--- croissant/healpix.py
+++ croissant/healpix.py
@@ -7,37 +7,63 @@ def nside2npix(nside):
     npix = 12 * nside**2
     return npix
 
+def check_shapes(npix, data, alm, frequencies):
+    data_check = True
+    alm_check = True
+    
+    if frequencies is None:
+        expected_data_shape = (npix,)
+    else:
+        nfreq = len(frequencies)
+        expected_data_shape = (nfreq, npix)
+        if alm is not None:
+            alm_check = np.shape(alm)[0] == nfreq
+        
+
+    if data is not None:
+        data_check = np.shape(data) == expected_data_shape
+
+    if not (data_check and alm_check):
+        raise ValueError("Shape mismatch in npix, data, alm, frequencies.")
 
 # nside's for which pixel weights exist
 PIX_WEIGHTS_NSIDE = [32, 64, 128, 256, 512, 1024, 2048, 4096]
 
 
 class HealpixBase:
-    def __init__(self, nside, data=None, alm=None, nested_input=False):
+    def __init__(
+            self,
+            nside,
+            data=None,
+            nested_input=False,
+            alm=None,
+            frequencies=None
+        ):
         hp.check_nside(nside, nest=nested_input)
         self.nside = nside
         self.npix = nside2npix(self.nside)
-        if nested_input and data is not None:
-            ix = hp.nest2ring(self.nside, np.arange(self.npix))
-            data = data[ix]
-        if data is None:
-            if alm is not None:
-                data = self.alm2hp()
-        else:
+        check_shapes(self.npix, data, alm, frequencies)
+
+        if data is not None:
+            if nested_input:
+                ix = hp.nest2ring(self.nside, np.arange(self.npix))
+                if frequencies is None:
+                    data = data[ix]
+                else:
+                    data = data[:, ix]
             if alm is None:
                 alm = self.hp2alm()
-            else:
-                if not np.allclose(alm, self.hp2alm()):
-                    warnings.warn(
-                        "Both a map and a list of alms are provided but"
-                        "they are not consistent.",
-                        UserWarning,
-                    )
+            elif not np.allclose(alm, self.hp2alm()):
+                raise ValueError("Provided alm and map are inconsistent.")
+        else:
+            if alm is not None:
+                data = self.alm2hp()
 
         self.data = data
         self.alm = alm
+        self.frequencies = frequencies
 
-    def hp2alm(self, lmax=None):
+    def alm2hp(self, lmax=None):
         if self.alm is None:
             raise ValueError("alm is None, cannot convert to map.")
         hp_map = hp.alm2map(self.alm, self.nside, lmax=lmax, mmax=lmax)
