2f62b0dbdab02b578df40fd3fbe636f28a07a33c
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-10 18:23:05 -0700

    initial commit

---
+++ croissant/beam.py
@@ -0,0 +1,32 @@
+from .healpix import HealpixBase, Alm
+
+class Beam(HealpixBase):
+    def __init__(self, power, frequencies=None, from_grid=False, **kwargs):
+        if from_grid:
+            req_kwargs = ["theta", "phi"]
+        else:
+            req_kwargs = ["nside"]
+
+        if not all([k in kwargs for k in req_kwargs]):
+            raise ValueError(f"Not all kwargs in {req_kwargs} are provided.")
+        
+        if from_grid:
+            theta = kwargs.pop["theta"]
+            phi = kwargs.pop["phi"]
+            super().from_grid()
+        else:
+            nside = kwargs.pop["nside"]
+            super().__init__(nside, data=power, frequencies=frequencies)
+        
+        self.power = self.data
+
+    @classmethod
+    def from_file(path):
+        raise NotImplementedError
+
+    def to_file(fname):
+        raise NotImplementedError
+
+class Sky(HealpixBase):
+    def __init__(self, temperature, frequencies=None, stokes=["I"]):
+        pass
