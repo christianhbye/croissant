0e3db0aab8d6de8ac098db9dcf4b0d9fd33ba787
Christian Hellum Bye <chbye@berkeley.edu>
2022-05-23 10:59:43 -0700

    clean up and fix bug

--- croissant/coordinates.py
+++ croissant/coordinates.py
@@ -1,8 +1,9 @@
-from astropy.coordinates import AltAz, EarthLocation, SkyCoord
+from astropy.coordinates import AltAz, EarthLocation
 from astropy.time import Time
 import astropy.units as u
 from healpy import Rotator
 import numpy as np
+
 
 def topo_to_radec(phi, theta, time, loc):
     """
@@ -19,31 +20,34 @@ def topo_to_radec(phi, theta, time, loc):
         theta = np.array(theta)
     # Allow loc to be earth location object and time to be Time object
     lat, lon = loc
-    loc = EarthLocation(lat=lat*u.deg, lon=lon*u.deg)
+    loc = EarthLocation(lat=lat * u.deg, lon=lon * u.deg)
     obstime = Time(time)
-    azs = [ph*u.degree for ph in phi]
-    alts = [(90-th)*u.degree for th in theta]
+    azs = [ph * u.degree for ph in phi]
+    alts = [(90 - th) * u.degree for th in theta]
     altaz = AltAz(alt=alts, az=azs, location=loc, obstime=obstime)
     icrs = altaz.transform_to("icrs")
     ra = icrs.ra.deg
     dec = icrs.dec.deg
     return ra, dec
 
-def _hp_rotate(from_coords, to_coords)
+
+def _hp_rotate(from_coords, to_coords):
     coords = {"galactic": "G", "ecliptic": "E", "equitorial": "C"}
     fc = from_coords.lower()
     tc = to_coords.lower()
     if not all([fc, tc] in coords):
         raise ValueError(
-            f"Invalid coordinate system name, must be in {list{coords.keys()}}"
+            f"Invalid coordinate system name, must be in {list[coords.keys()]}"
         )
     rot = Rotator(coord=[coords[fc], coords[tc]])
     return rot
+
 
 def rotate_map(sky_map, from_coords="galactic", to_coords="equitorial"):
     rot = _hp_rotate(from_coords, to_coords)
     rotated_map = rot.rotate_map_alms(sky_map)
     return rotated_map
+
 
 def rotate_alm(alm, from_coords="galactic", to_coords="equitorial"):
     rot = _hp_rotate(from_coords, to_coords)
