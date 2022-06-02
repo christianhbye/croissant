67b510a66a105e98cd3727e17b06deedb1996cab
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-02 16:37:53 -0700

    use new angle2alm function

--- croissant/simulator.py
+++ croissant/simulator.py
@@ -4,12 +4,11 @@ from astropy.time import Time
 import matplotlib.pyplot as plt
 from multiprocessing import Pool
 import numpy as np
-from scipy.interpolate import RectSphereBivariateSpline
 import warnings
 
 from . import dpss
 from .coordinates import topo_to_radec
-from .healpix import Alm, grid2alm
+from .healpix import Alm, angle2alm
 
 
 class Simulator:
@@ -17,9 +16,8 @@ class Simulator:
         self,
         beam,
         sky,
-        obs_lat,
-        obs_lon,
-        obs_alt,
+        lmax,
+        obs_loc,
         t_start,
         t_end=None,
         N_times=None,
@@ -30,18 +28,18 @@ class Simulator:
         """
         Simulator class. Prepares and runs simulations.
         """
+        self.lmax = lmax
         # set up frequencies to run the simulation at
         if frequencies is None:
             frequencies = sky.frequencies
         self.frequencies = frequencies
         # set up the location of the telescope as astropy.EarthLocation object
+        lat, lon, alt = obs_loc
         self.loc = EarthLocation(
-            lat=obs_lat * units.deg,
-            lon=obs_lon * units.deg,
-            height=obs_alt * units.m,
+            lat=lat * units.deg, lon=lon * units.deg, height=alt * units.m
         )
         # set up observing time as astropy.Time object
-        self.t_start = Time(t_start, location=self.loc)
+        self.t_start = Time(t_start, location=self.loc, scale="utc")
         if delta_t is not None:
             try:
                 delta_t = delta_t.to_value(units.s)
@@ -67,24 +65,18 @@ class Simulator:
         beam.horizon_cut(horizon=horizon)
         self.beam = beam
         # initialize sky
-        self.sky = Alm.from_healpix(sky, lmax=self.beam.lmax)
+        self.sky = Alm.from_healpix(sky, lmax=self.lmax)
         if self.sky.coords != "equitorial":
             self.sky.switch_coords("equitorial")
 
     def _prepare_beam(self):
         # rotate to ra/dec at first observing time given location
-        ph, th = self.beam.phi, self.beam.theta
-        ra, dec = topo_to_radec(ph, th, self.t_start, self.loc)
-        # interpolate to ra/decs to get even sampling
+        phi = self.beam.phi.copy()
+        theta = self.beam.theta.copy()
+        ra, dec = topo_to_radec(phi, theta, self.t_start, self.loc)
         dec = np.pi / 2 - dec  # colatitude, [0, pi]
-        ra -= np.pi  # move to [-pi, pi)
-        smooth_dec = np.linspace(0, np.pi, 181)
-        smooth_ra = np.linspace(-np.pi, np.pi, 360, endpoint=False)
-        interp = RectSphereBivariateSpline(dec, ra, self.beam.data)
-        interp_beam = interp(smooth_dec, smooth_ra)
         # compute alms of beam from ra/dec
-        smooth_ra += np.pi  # must be 0-2pi for scipy spherical harms
-        alm = grid2alm(interp_beam, smooth_dec, smooth_ra, lmax=self.lmax)
+        alm = angle2alm(self.beam.data, dec, ra, lmax=self.lmax)
         self.beam.alm = alm
 
     def _compute_dpss(self, nterms=10):
@@ -127,8 +119,10 @@ class Simulator:
         """
         Compute the convolution for a range of times.
         """
+        print("prep beam")
         self._prepare_beam()
         nterms = kwargs.pop("nterms", None)
+        print("compute dpss")
         self._compute_dpss(nterms=nterms)
         if nterms is None:
             nterms = self.beam.coeffs.shape[0]
@@ -149,9 +143,11 @@ class Simulator:
                         callback=get_res,
                     )
 
-        for i, t in enumerate(self.times):
-            conv = self._run_onetime(t)[1]
-            waterfall_dpss[i] = conv
+        else:
+            for i, t in enumerate(self.times):
+                print(f"{t=}")
+                conv = self._run_onetime(t)[1]
+                waterfall_dpss[i] = conv
 
         # convert back to frequency
         self.waterfall = self.design_matrix @ waterfall_dpss.T
