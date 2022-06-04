1c5d1c4fa7b7fb642e8f576b07f5931905dc6eb2
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-03 18:35:07 -0700

    add coordinate conversion methods

--- croissant/coordinates.py
+++ croissant/coordinates.py
@@ -1,11 +1,11 @@
 from astropy.coordinates import AltAz, EarthLocation, ICRS
 from astropy.time import Time
 from astropy import units
-from healpy import Rotator
+from healpy import nside2npix, pix2ang, Rotator
 import numpy as np
 
 
-def topo_to_radec(phi, theta, time, loc, grid=True):
+def topo2radec(phi, theta, time, loc, grid=True):
     """
     Convert topocentric coordinates to ra/dec at given time. Useful for
     antenna beams.
@@ -54,6 +54,72 @@ def topo_to_radec(phi, theta, time, loc, grid=True):
     dec = icrs.dec.rad
     return ra, dec
 
+
+def radec2topo(ra, dec, time, loc):
+    """
+    Convert the ra/dec-combination(s) to topocentric coordinates.
+
+    Parameters
+    ----------
+    ra : array-like
+        The right ascension(s) in degrees.
+    dec : array-like
+        The declination(s) in degrees.
+    time : array-like, str, or astropy.time.Time object
+        The time to compute the transformation at. Must be able to initialize
+        an astropy.time.Time object.
+    loc : array-like or astropy.coordinates.EarthLocation object
+        The location of the topocentric coordinates. Must be able to initialize
+        an astropy.coordinates.EarthLocation object.
+
+    Returns
+    -------
+    theta : np.ndarray
+        Colatitudes in radians.
+    phi : np.ndarray
+        Azimuths in radians.
+
+    """
+    if type(loc) != EarthLocation:
+        lat, lon, alt = loc
+        loc = EarthLocation(
+            lat=lat*units.deg, lon=lon*units.deg, height=alt*units.m
+        )
+    if type(time) != Time:
+        time = Time(time, scale="utc", location=loc)
+
+    icrs = ICRS(ra=ra*units.deg, dec=dec*units.deg)
+    # transform to altaz
+    altaz = icrs.transform_to(AltAz(location=loc, obstime=time))
+    theta = np.pi/2 - altaz.alt.rad
+    phi = altaz.az.rad
+    return theta, phi
+
+def healpix2lonlat(nside, pix=None)
+    """
+    Compute the longtitudes and latitudes of the pixel centers of a healpix
+    map.
+
+    Parameters
+    ----------
+    nside : int
+        The nside of the healpix map.
+    pix : array-like (optional)
+        Which pixels to get the longtitudes and latitudes of. Defaults to all
+        pixels.
+
+    Returns
+    -------
+    lon : scalar or np.ndarray
+        The longtitude(s) in degrees.
+    lat : scalar or np.ndarray
+        The latitude(s) in degrees.
+
+    """
+    if pix is None:
+        pix = np.arange(nside2npix(nside))
+    lon, lat = pix2ang(nside, pix, nest=False, lonlat=True)
+    return lon, lat
 
 def _hp_rotate(from_coords, to_coords):
     coords = {"galactic": "G", "ecliptic": "E", "equitorial": "C"}
