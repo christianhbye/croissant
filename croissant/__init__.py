618229b696db79d86c30332884f72b82c03c496e
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-08 14:13:14 -0700

    add list of nsides with pix weights to constants

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "2.0.0"
 
-from . import beam, dpss, healpix, sky, simulator
+from . import beam, constants, dpss, healpix, sky, simulator
--- croissant/constants.py
+++ croissant/constants.py
@@ -1,1 +1,3 @@
-sidereal_day = 86164.0905  # seconds
+sidereal_day = 86164.0905  # sidereal day in seconds
+# nside's for which healpix has computed pixel weights:
+PIX_WEIGHTS_NSIDE = [32, 64, 128, 256, 512, 1024, 2048, 4096]
