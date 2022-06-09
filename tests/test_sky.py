7205dcdc4978d29135f35f075f31283b2efd0bd2
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-08 22:22:02 -0700

    simplify sky and test sky, remove unnecessary functionality

--- tests/test_sky.py
+++ tests/test_sky.py
@@ -1,20 +1,22 @@
 import healpy as hp
 import numpy as np
 
-from croissant import sky
+from croissant.sky import Sky
 
 
 def test_power_law_map():
-    nside = 1
+    nside = 32
     npix = hp.nside2npix(nside)
-    freq = 1
-    data = np.arange(npix).reshape(1, -1)
-    test_sky = sky.Sky(data, frequencies=freq)
+    freq = 25.0
+    data = np.arange(npix)
+    sky = Sky(data, frequencies=freq)
     freq_out = np.linspace(1, 50)
-    beta = 3
-    test_sky.power_law_map(freq_out, spectral_index=beta)
-    assert np.allclose(test_sky.frequencies, freq_out)
+    beta = -2.5
+    sky.power_law_map(freq_out, spectral_index=beta)
+    assert np.allclose(sky.frequencies, freq_out)
     for pix in range(npix):  # check that there's a power law at each pixel
-        temp = test_sky.data[:, pix]
-        pl = data[0, pix] * freq_out**beta
+        temp = sky.data[:, pix]
+        f_ratio = freq_out / freq
+        f_ratio.shape = (1, -1)
+        pl = data[pix] * f_ratio**beta
         assert np.allclose(temp, pl)
