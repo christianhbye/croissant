dabc8d1103a2cabf99769bab54b1e3f44e1431dc
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-15 12:00:36 -0700

    rename coordinates -> rotations, add test for new rotate alm function

--- tests/test_rotations.py
+++ tests/test_rotations.py
@@ -4,7 +4,7 @@ from astropy import units
 import healpy as hp
 import numpy as np
 
-from croissant import coordinates as coord
+from croissant import rotations
 
 
 def test_topo2radec():
@@ -14,7 +14,7 @@ def test_topo2radec():
         lat=40.5 * units.deg, lon=130.2 * units.deg, height=10.3 * units.m
     )
     time = "2022-06-09 16:12:00"
-    ra, dec = coord.topo2radec(theta, phi, time, loc, grid=True)
+    ra, dec = rotations.topo2radec(theta, phi, time, loc, grid=True)
     assert -90 <= dec.all() < 90
     assert 0 <= ra.all() < 360
     az = phi.reshape(1, -1) * units.rad
@@ -26,7 +26,7 @@ def test_topo2radec():
 
     # change phi/theta from coord axes to datapoints
     phi, theta = [ang.ravel() for ang in np.meshgrid(phi, theta)]
-    ra, dec = coord.topo2radec(theta, phi, time, loc, grid=False)
+    ra, dec = rotations.topo2radec(theta, phi, time, loc, grid=False)
     assert -90 <= dec.all() < 90
     assert 0 <= ra.all() < 360
     assert np.allclose(ra, icrs.ra.rad.ravel())
@@ -34,7 +34,7 @@ def test_topo2radec():
 
     # pass astropy time to top2radec:
     time = Time(time)
-    ra, dec = coord.topo2radec(theta, phi, time, loc, grid=False)
+    ra, dec = rotations.topo2radec(theta, phi, time, loc, grid=False)
     assert -90 <= dec.all() < 90
     assert 0 <= ra.all() < 360
     assert np.allclose(ra, icrs.ra.rad.ravel())
@@ -44,8 +44,10 @@ def test_topo2radec():
     phi = np.linspace(0, 2 * np.pi, num=360, endpoint=False)
     theta = np.linspace(0, np.pi, num=181)[1:-1]  # remove poles
     phi, theta = [ang.ravel() for ang in np.meshgrid(phi, theta)]
-    ra, dec = np.rad2deg(coord.topo2radec(theta, phi, time, loc, grid=False))
-    theta_, phi_ = coord.radec2topo(ra, dec, time, loc)
+    ra, dec = np.rad2deg(
+        rotations.topo2radec(theta, phi, time, loc, grid=False)
+    )
+    theta_, phi_ = rotations.radec2topo(ra, dec, time, loc)
     # handle precision issue
     phi_ = np.where(phi_ >= 2 * np.pi - 1e-10, phi_ - 2 * np.pi, phi_)
     assert np.allclose(theta, theta_)
@@ -60,7 +62,7 @@ def test_radec2topo():
         lat=40.5 * units.deg, lon=130.2 * units.deg, height=10.3 * units.m
     )
     time = "2022-06-09 16:12:00"
-    theta, phi = coord.radec2topo(ra, dec, time, loc)
+    theta, phi = rotations.radec2topo(ra, dec, time, loc)
     assert 0 <= theta.all() < np.pi / 2
     assert 0 <= phi.all() < 2 * np.pi
     icrs = ICRS(ra=ra * units.deg, dec=dec * units.deg)
@@ -70,7 +72,7 @@ def test_radec2topo():
 
     # use astropy time
     time = Time(time)
-    theta, phi = coord.radec2topo(ra, dec, time, loc)
+    theta, phi = rotations.radec2topo(ra, dec, time, loc)
     assert 0 <= theta.all() < np.pi / 2
     assert 0 <= phi.all() < 2 * np.pi
     assert np.allclose(theta, np.pi / 2 - aa.alt.rad)
@@ -81,8 +83,10 @@ def test_radec2topo():
     dec = np.linspace(90, -90, num=181)
     dec = dec[1:-1]  # remove poles since it will mess with ra
     ra, dec = [ang.ravel() for ang in np.meshgrid(ra, dec)]
-    theta, phi = coord.radec2topo(ra, dec, time, loc)
-    ra_, dec_ = np.rad2deg(coord.topo2radec(theta, phi, time, loc, grid=False))
+    theta, phi = rotations.radec2topo(ra, dec, time, loc)
+    ra_, dec_ = np.rad2deg(
+        rotations.topo2radec(theta, phi, time, loc, grid=False)
+    )
     # handle precision issue
     ra_ = np.where(ra_ >= 360 - 1e-10, ra_ - 360, ra_)
     assert np.allclose(dec, dec_)
@@ -91,7 +95,7 @@ def test_radec2topo():
 
 def test_hp_rotate():
     # verify that this method yields the same as the healpy example in the docs
-    rot = coord.hp_rotate("galactic", "ecliptic")
+    rot = rotations.hp_rotate("galactic", "ecliptic")
     theta_gal, phi_gal = np.pi / 2, 0
     theta_ecl, phi_ecl = rot(theta_gal, phi_gal)
     assert np.isclose(theta_ecl, 1.66742347999)  # from healpy docs
@@ -106,7 +110,9 @@ def test_rotate_map():
     nside = 8
     npix = hp.nside2npix(nside)
     m = np.arange(npix)
-    rm = coord.rotate_map(m, from_coords="galactic", to_coords="equatorial")
+    rm = rotations.rotate_map(
+        m, from_coords="galactic", to_coords="equatorial"
+    )
     hp_rot = hp.Rotator(coord=["G", "C"])
     hprm = hp_rot.rotate_map_alms(m, use_pixel_weights=False)
     assert np.allclose(rm, hprm)
@@ -115,7 +121,9 @@ def test_rotate_map():
     nside = 64
     npix = hp.nside2npix(nside)
     m = np.arange(npix)
-    rm = coord.rotate_map(m, from_coords="equatorial", to_coords="ecliptic")
+    rm = rotations.rotate_map(
+        m, from_coords="equatorial", to_coords="ecliptic"
+    )
     hp_rot = hp.Rotator(coord=["C", "E"])
     hprm = hp_rot.rotate_map_alms(m, use_pixel_weights=True)
     assert np.allclose(rm, hprm)
@@ -126,7 +134,9 @@ def test_rotate_map():
     # several maps at once
     f = np.linspace(1, 50, 50).reshape(-1, 1)
     maps = m.reshape(1, -1) * f**3.2
-    rm = coord.rotate_map(maps, from_coords="galactic", to_coords="equatorial")
+    rm = rotations.rotate_map(
+        maps, from_coords="galactic", to_coords="equatorial"
+    )
     hp_rot = hp.Rotator(coord=["G", "C"])
     hprm = np.empty((f.size, npix))
     for i, m in enumerate(maps):
@@ -138,7 +148,7 @@ def test_rotate_alm():
     lmax = 10
     size = hp.Alm.getsize(lmax)
     alm = np.arange(size) + 5j * np.arange(size) ** 2
-    rot_alm = coord.rotate_alm(
+    rot_alm = rotations.rotate_alm(
         alm, from_coords="galactic", to_coords="equatorial"
     )
     hp_rot = hp.Rotator(coord=["G", "C"])
@@ -150,10 +160,31 @@ def test_rotate_alm():
     f = np.linspace(1, 50, 50).reshape(-1, 1)
     alms = np.repeat(alm, f.size, axis=0) * f**2
     assert alms.shape == (50, size)
-    rot_alms = coord.rotate_alm(
+    rot_alms = rotations.rotate_alm(
         alms, from_coords="galactic", to_coords="equatorial"
     )
     hp_rot_alms = np.empty_like(alms)
     for i, a in enumerate(alms):
         hp_rot_alms[i] = hp_rot.rotate_alm(a)
     assert np.allclose(rot_alms, hp_rot_alms)
+
+
+def test_rot_alm_z():
+    lmax = 10
+    phi = np.pi / 2
+    phase = rotations.rot_alm_z(phi, lmax)
+    for ell in range(lmax + 1):
+        for emm in range(ell + 1):
+            ix = hp.Alm.getidx(lmax, ell, emm)
+            assert np.isclose(phase[ix], np.exp(1j * emm * phi))
+
+    # rotate a set of angles
+    phi = np.linspace(0, 2 * np.pi, num=361)  # 1 deg spacing
+    phase = rotations.rot_alm_z(phi, lmax)
+    for ell in range(lmax + 1):
+        for emm in range(ell + 1):
+            ix = hp.Alm.getidx(lmax, ell, emm)
+            assert np.allclose(phase[:, ix], np.exp(1j * emm * phi))
+
+    # check that phi = 0 and phi = 2pi give the same answer
+    assert np.allclose(phase[0], phase[-1])
