9434004616d272edf2c35a4a6f396ce6a8514000
Christian Hellum Bye <chbye@berkeley.edu>
2022-06-15 11:59:40 -0700

    rename coordinates -> rotations

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,4 +1,4 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "2.0.0-dev"
 
-from . import beam, constants, dpss, healpix, sky, simulator
+from . import beam, constants, dpss, healpix, rotations, sky, simulator
