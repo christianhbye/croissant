83492900f135f5f36cea8ad63386471120b5c6e9
Christian Hellum Bye <chbye@berkeley.edu>
2023-02-14 12:26:43 -0800

    import new sphtransform file

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,7 +1,7 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "3.0.0"
 
-from . import constants, dpss, healpix, simulator
+from . import constants, dpss, healpix, simulator, sphtransform
 from .beam import Beam
-from .rotation import Rotator
+from .rotations import Rotator
 from .sky import Sky
