679cbbd90b2aae447d4032167df8e10ab38ff095
Christian Hellum Bye <chbye@berkeley.edu>
2023-02-14 11:07:19 -0800

    import Rotator from rotations

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,6 +1,7 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "3.0.0"
 
-from . import constants, dpss, healpix, rotations, simulator
+from . import constants, dpss, healpix, simulator
 from .beam import Beam
+from .rotation import Rotator
 from .sky import Sky
