947355aee7984bf1acd0ed72e86f3f9a415f36db
Christian Hellum Bye <chbye@berkeley.edu>
2023-02-14 17:19:09 -0800

    fix imports in init

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,7 +1,8 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "3.0.0"
 
-from . import constants, dpss, healpix, simulator, sphtransform
+from . import constants, dpss, simulator, sphtransform
+from .healpix import Alm, HealpixMap
 from .beam import Beam
 from .rotations import Rotator
 from .sky import Sky
