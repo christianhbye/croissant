ee6e7d15e9e94d70d7352930bc3c9998e2eb0d8c
Christian Hellum Bye <chbye@berkeley.edu>
2023-02-15 17:38:23 -0800

    import Simulator in init

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,8 +1,9 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "3.0.0"
 
-from . import constants, dpss, simulator, sphtransform
+from . import constants, dpss, sphtransform
 from .healpix import Alm, HealpixMap
 from .beam import Beam
 from .rotations import Rotator
+from .simulator import Simulator
 from .sky import Sky
