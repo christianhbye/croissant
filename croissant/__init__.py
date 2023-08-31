611b28daafba99cfabd21012b7718531136ed362
Christian Hellum Bye <chbye@berkeley.edu>
2023-08-31 14:45:36 -0700

    enable double for jax

--- croissant/__init__.py
+++ croissant/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Christian Hellum Bye"
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 from . import constants, dpss, sphtransform
 from .healpix import Alm, HealpixMap
@@ -7,3 +7,7 @@ from .beam import Beam
 from .rotations import Rotator
 from .simulator import Simulator
 from .sky import Sky
+
+# enable double precision
+from jax import config
+config.update("jax_enable_x64", True)
