70c944edee96019d2f0f06a6cfdbdfe8be9060ab
Christian Hellum Bye <chbye@berkeley.edu>
2023-09-12 16:06:53 -0700

    initial commit

---
+++ croissant/crojax/sky.py
@@ -0,0 +1,35 @@
+from functools import partial
+import jax
+import s2fft
+from pygdsm import GlobalSkyModel2016 as GSM16
+from .healpix import Alm
+
+
+class Sky(Alm):
+    @classmethod
+    def gsm(cls, freq, lmax):
+        """
+        Construct a sky object with pygdsm.
+
+        Parameters
+        ----------
+        freq : jnp.ndarray
+            Frequencies to make map at in MHz.
+        lmax : int
+            Maximum multipole to compute alm up to.
+        """
+        gsm = GSM16(freq_unit="MHz", data_unit="TRJ", resolution="lo")
+        sky_map = gsm.generate(freq)
+        forward = partial(
+            s2fft.forward_jax,
+            spin=0,
+            nside=gsm.nside,
+            reality=True,
+            precomps=None,
+            spmd=True,
+            L_lower=0,
+        )
+        L = lmax + 1
+        sky_alm = jax.vmap(forward, in_axes=[0, None])(sky_map, L)
+        obj = cls(sky_alm, frequencies=freq, coord="G")
+        return obj
