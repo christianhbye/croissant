0b22b00bd54a8c8d34abd5e80fcb45ba7fc9d1d2
Christian Hellum Bye <chbye@berkeley.edu>
2024-02-02 14:52:31 -0800

    --no-edit

--- croissant/constants.py
+++ croissant/constants.py
@@ -1,7 +1,4 @@
-import numpy as np
-
-# nside's for which healpix has computed pixel weights:
-PIX_WEIGHTS_NSIDE = (32, 64, 128, 256, 512, 1024, 2048, 4096)
+from math import pi, sqrt
 
 # sidereal days in seconds
 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
@@ -9,4 +6,4 @@ sidereal_day_earth = 23.9345 * 3600
 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/moonfact.html
 sidereal_day_moon = 655.720 * 3600
 
-Y00 = 1 / np.sqrt(4 * np.pi)  # the 0,0 spherical harmonic function
+Y00 = 1 / sqrt(4 * pi)  # the 0,0 spherical harmonic function
