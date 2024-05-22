9e706497991f41698ef64623ae55bcccd9d642d0
Christian Hellum Bye <chbye@berkeley.edu>
2024-05-22 16:40:22 -0700

    shift from classes to pure functions

--- croissant/jax/simulator.py
+++ croissant/jax/simulator.py
@@ -1,21 +1,57 @@
+from functools import partial
 import jax
 import jax.numpy as jnp
-from ..simulatorbase import SimulatorBase
+
+from .. import constants
+
+
+@partial(jax.jit, static_argnums=(0,))
+def rot_alm_z(lmax, times, sidereal_day=constants.sidereal_day_moon):
+    """
+    Compute the complex phases that rotate the sky for a range of times. The
+    first time is the reference time and the phases are computed relative to
+    this time.
+
+    Parameters
+    ----------
+    lmax : int
+        The maximum ell value.
+    times : jnp.ndarray
+        The times for which to compute the phases.
+    sidereal_day : str
+        The length of a sidereal day in the same units as ``times''. Default
+        is the sidereal day of the Moon, see constants.py for the sidereal
+        day of the Earth.
+
+    Returns
+    -------
+    phases : jnp.ndarray
+        The phases that rotate the sky, of the form exp(-i*m*phi(t)).
+        Shape (N_times, 2*lmax+1).
+
+    """
+    dt = times - times[0]  # time difference from reference
+    phi = 2 * jnp.pi * dt / sidereal_day  # rotation angle
+    emms = jnp.arange(-lmax, lmax + 1)  # m values
+    phases = jnp.exp(-1j * emms[None] * phi[:, None])
+    return phases
+
 
 @jax.jit
-def convolve(sky_alm, beam_alm, phases):
+def run(sky_alm, beam_alm, phases):
     """
     Compute the convolution for a range of times in jax. The convolution is
     a dot product in l,m space. Axes are in the order: time, freq, ell, emm.
-    
+
     Parameters
     ----------
     sky_alm : jnp.ndarray
         The sky alms. Shape (N_freqs, lmax+1, 2*lmax+1).
     beam_alm : jnp.ndarray
-        The beam alms. Shape (N_freqs, lmax+1, 2*lmax+1).
+        The beam alms. Shape (N_freqs, lmax+1, 2*lmax+1). The beam should be
+        normalized to have total power of unity.
     phases : jnp.ndarray
-        The phases that roate the sky, of the form exp(-i*m*phi(t)).
+        The phases that rotate the sky, of the form exp(-i*m*phi(t)).
         Shape (N_times, 2*lmax+1).
 
     Returns
@@ -28,29 +64,3 @@ def convolve(sky_alm, beam_alm, phases):
     b = beam_alm.conjugate()[None, :, :, :]  # add time axis and conjugate
     res = jnp.sum(s * p * b, axes=(2, 3))  # dot product in l,m space
     return res
-
-def convolve_dpss():
-    raise NotImplementedError
-
-class Simulator(SimulatorBase):
-    def run(self, dpss=True, **dpss_kwargs):
-        """
-        Compute the convolution for a range of times in jax.
-        
-        Parameters
-        ----------
-        dpss : bool
-            Whether to use a dpss basis or not.
-        dpss_kwargs : dict
-            Passed to SimulatorBase().compute_dpss.
-
-        """
-        if dpss:
-            res = convolve_dpss()
-        else:
-            res = convolve(
-                self.sky.alm,
-                self.beam.alm,
-                self.sky.rot_alm_z(self.dt, self.world)
-            )
-        self.waterfall = res / self.beam.total_power
