717f2593419071ff8599192be6e16d23d9fbeb06
Christian Hellum Bye <chbye@berkeley.edu>
2024-05-22 16:40:07 -0700

    move functions to alm.py, shift from classes to pure functions

--- croissant/jax/__init__.py
+++ croissant/jax/__init__.py
@@ -3,7 +3,5 @@ from jax import config
 
 config.update("jax_enable_x64", True)
 
-from .beam import Beam
-from .healpix import Alm
-from .simulator import Simulator
+from . import alm, simulator
 from .sky import Sky
