e85b2ba65046e612c5f0a12db48dcfc664a1e483
Christian Hellum Bye <chbye@berkeley.edu>
2024-06-01 16:58:47 -0700

    change rotation.py to generate euler angles and wigner functions, which are passed to s2fft

--- croissant/jax/rotations.py
+++ croissant/jax/rotations.py
@@ -1,134 +1,30 @@
-from astropy.coordinates import AltAz
-from lunarsky import LunarTopo
-from s2fft import rotate_flms
+from s2fft import generate_rotate_dls
 from ..utils import get_rot_mat, rotmat_to_euler
-from .alm import lmax_from_shape
 
 
-def rotate_alm(alm, from_frame, to_frame, dl_array=None):
+def generate_euler_dl(lmax, from_frame, to_frame):
     """
-    Transform a spherical harmonic decomposition from one coordinate system to
-    another. This is a wrapper around the s2fft.rotate_flms function that
-    computes the Euler angles from the input and output coordinate systems.
+    Generate the Euler angles and reduced Wigner d-function values for a
+    coordinate transformation.
 
     Parameters
     ----------
-    alm : jnp.ndarray
-        The alm array to transform.
+    lmax : int
+        The maximum spherical harmonic degree.
     from_frame : str or astropy frame
         The coordinate system of the input alm.
     to_frame : str or astropy frame
         The coordinate system of the output alm.
-    dl_array : jnp.ndarray
-        Precomputed array of reduced Wigner d-function values. These
-        can be computed with the s2fft.generate_rotate_dls function.
 
     Returns
     -------
-    alm_rot : jnp.ndarray
-        The alm array in the ``to_frame'' coordinate system.
+    euler : jnp.ndarray
+        The Euler angles for the coordinate transformation.
+    dl_array : jnp.ndarray
+        The reduced Wigner d-function values for the coordinate transformation.
 
     """
     rmat = get_rot_mat(from_frame, to_frame)
     euler = rotmat_to_euler(rmat, eulertype="ZYZ")
-    lmax = lmax_from_shape(alm.shape)
-    alm_rot = rotate_flms(alm, lmax + 1, euler, dl_array=dl_array)
-    return alm_rot
-
-
-def gal2eq(alm, dl_array=None):
-    """
-    Transform a spherical harmonic decomposition from Galactic to Equatorial
-    coordinates.
-
-    Parameters
-    ----------
-    alm : jnp.ndarray
-        The alm array to transform.
-    dl_array : jnp.ndarray
-        Precomputed array of reduced Wigner d-function values. These
-        can be computed with the s2fft.generate_rotate_dls function.
-
-    Returns
-    -------
-    alm_rot : jnp.ndarray
-        The alm array in Equatorial coordinates.
-
-    """
-    return rotate_alm(alm, "galactic", "fk5", dl_array=dl_array)
-
-
-def gal2mcmf(alm, dl_array=None):
-    """
-    Transform a spherical harmonic decomposition from Galactic to MCMF
-    coordinates (moon equivalent of equatorial coordinates).
-
-    Parameters
-    ----------
-    alm : jnp.ndarray
-        The alm array to transform.
-    dl_array : jnp.ndarray
-        Precomputed array of reduced Wigner d-function values. These
-        can be computed with the s2fft.generate_rotate_dls function.
-
-    Returns
-    -------
-    alm_rot : jnp.ndarray
-        The alm array in MCMF coordinates.
-
-    """
-    return rotate_alm(alm, "galactic", "mcmf", dl_array=dl_array)
-
-
-def topo2eq(alm, loc, time, dl_array=None):
-    """
-    Transform a spherical harmonic decomposition from topocentric on Earth to
-    equatorial coordinates.
-
-    Parameters
-    ----------
-    alm : jnp.ndarray
-        The alm array to transform.
-    loc : astropy.coordinates.EarthLocation
-        The location of the observer.
-    time : astropy.time.Time
-        The time of the observation.
-    dl_array : jnp.ndarray
-        Precomputed array of reduced Wigner d-function values. These
-        can be computed with the s2fft.generate_rotate_dls function.
-
-    Returns
-    -------
-    alm_rot : jnp.ndarray
-        The alm array in Equatorial coordinates.
-
-    """
-    topo = AltAz(location=loc, obstime=time)
-    return rotate_alm(alm, topo, "fk5", loc, time, dl_array=dl_array)
-
-
-def topo2mcmf(alm, loc, time, dl_array=None):
-    """
-    Transform a spherical harmonic decomposition from topocentric on Moon to
-    equatorial coordinates.
-
-    Parameters
-    ----------
-    alm : jnp.ndarray
-        The alm array to transform.
-    loc : lunarsky.MoonLocation
-        The location of the observer.
-    time : lunarsky.Time
-        The time of the observation.
-    dl_array : jnp.ndarray
-        Precomputed array of reduced Wigner d-function values. These
-        can be computed with the s2fft.generate_rotate_dls function.
-
-    Returns
-    -------
-    alm_rot : jnp.ndarray
-        The alm array in MCMF coordinates.
-
-    """
-    topo = LunarTopo(location=loc, obstime=time)
-    return rotate_alm(alm, topo, "mcmf", loc, time, dl_array=dl_array)
+    dl_array = generate_rotate_dls(lmax, euler[1])
+    return euler, dl_array
