dd43e1e7ec8aad9e1abc0803061e14e99e17b832
Christian Hellum Bye <chbye@berkeley.edu>
2026-02-19 13:03:25 -0800

    change project structure

---
+++ src/croissant/__init__.py
@@ -0,0 +1,9 @@
+__author__ = "Christian Hellum Bye"
+__version__ = "5.0.0"
+
+from . import constants
+from . import core
+from . import dpss
+from . import jax
+from . import utils
+from .core import *  # noqa F403
