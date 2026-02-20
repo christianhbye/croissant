5a42cd854187422ee4dbbb1a80e0ba1afe141be1
Christian Hellum Bye <chbye@berkeley.edu>
2026-02-19 21:00:07 -0800

    remove unused function coord_rep

--- src/croissant/utils.py
+++ src/croissant/utils.py
@@ -35,29 +35,6 @@ def _future_warning(func):
     return wrapper
 
 
-def coord_rep(coord):
-    """
-    Shorthand notation for coordinate systems.
-
-    Parameters
-    ----------
-    coord : str
-        The name of the coordinate system.
-
-    Returns
-    -------
-    rep : str
-        The one-letter shorthand notation for the coordinate system.
-
-    """
-    coord = coord.upper()
-    if coord[0] == "E" and coord[1] == "Q":
-        rep = "C"
-    else:
-        rep = coord[0]
-    return rep
-
-
 @_future_warning
 def get_rot_mat(from_frame, to_frame):
     """
