7a7a0eeb2bb5350c8490f5185e359ea9d3a4a494
Christian Hellum Bye <chbye@berkeley.edu>
2026-02-19 22:01:38 -0800

    fix imports, disable jax double precision config

--- src/croissant/__init__.py
+++ src/croissant/__init__.py
@@ -1,11 +1,6 @@
 __author__ = "Christian Hellum Bye"
 __version__ = "5.0.0"
 
-# enable double precision
-from jax import config
-
-config.update("jax_enable_x64", True)
-
 from . import alm, constants, rotations, simulator, utils
 from .beam import Beam
-from .simulator import Simulator
+from .sky import Sky
