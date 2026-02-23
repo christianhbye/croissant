53541a94ecde5c740d8d0e48bf2e57f7df1c3bbe
Christian Hellum Bye <chbye@berkeley.edu>
2026-02-23 09:04:13 -0800

    fix: maintain consistent function signatures in croissant top level for backwards compat

--- src/croissant/constants.py
+++ src/croissant/constants.py
@@ -1,4 +1,6 @@
-import jax.numpy as jnp
+import warnings
+
+import numpy as np
 
 # sidereal days in seconds
 # https://nssdc.gsfc.nasa.gov/planetary/factsheet/earthfact.html
@@ -8,4 +10,18 @@ sidereal_day_moon = 655.720 * 3600
 
 sidereal_day = {"earth": sidereal_day_earth, "moon": sidereal_day_moon}
 
-Y00 = 1 / jnp.sqrt(4 * jnp.pi)  # the 0,0 spherical harmonic function
+Y00 = 1 / np.sqrt(4 * np.pi)  # the 0,0 spherical harmonic function
+
+
+def _get_pix_weigths():
+    warnings.warn(
+        "The constant PIX_WEIGHTS_NSIDE is deprecated and will be removed in "
+        "a future version. It was used for healpy routines which are no "
+        "longer used in croissant",
+        DeprecationWarning,
+        stacklevel=2,
+    )
+    return (32, 64, 128, 512, 1024, 2048, 4096)
+
+
+PIX_WEIGHTS_NSIDE = _get_pix_weigths()
